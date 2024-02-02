46fc8543bf99f1a5d3f1c6703685c4a9c8393a87
Lu-el <lubov.elunina@mail.ru>
2024-02-02 19:18:47 +0400

    lesson 04-01

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { Header } from './modules/header/header';
 import { Main } from './modules/main/main';
 import { Footer } from './modules/footer/footer';
 import { Order } from './modules/order/order';
+import { ProductList } from './modules/productList/productList';
 
 // import { Navigation, Thumbs } from 'swiper/modules';
 // import Swiper  from 'swiper';
@@ -52,14 +53,40 @@ const init = () => {
 
   router
     .on("/", () => {
-      console.log('на главной');
-    })
+      new ProductList().mount(new Main().element, [1])
+    },
+      {
+        leave(done) {
+          console.log('leave');
+          done()
+        },
+
+        already() {
+          console.log('already');
+        },
+      })
     .on('/category', () => {
-      console.log('category')
-    })
-    .on('/favorite', () => {
-      console.log('favorite')
-    })
+      new ProductList().mount(new Main().element, [1, 2, 3, 4, 5, 6], 'Категория')
+    },
+      {
+        leave(done) {
+          console.log('leave');
+          done()
+        }
+      })
+    .on('/favorite', 
+    () => {
+      new ProductList().mount(
+        new Main().element, 
+        [1, 2, 3], 
+        'Favor')
+    },
+      {
+        leave(done) {
+          console.log('leave');
+          done()
+        }
+      })
     .on('/search', () => {
       console.log('')
     })
@@ -74,8 +101,24 @@ const init = () => {
       console.log('order')
     })
     .notFound(() => {
-      console.log('404')
-    })
+      new Main().element.innerHTML = 
+      `<h2>Страница не найдена</h2>
+      <p>Через 5 сек вы будете перенаправлены 
+        <a href="/">на главную страницу</a>
+      </p>`;
+
+      setTimeout(() => {
+        router.navigate("/");
+      }, 5000)      
+    },
+    {
+      leave(done) {
+        new Main().element.innerHTML = ``;
+        console.log('leave');
+        done()
+      }
+    }
+    )
 
   router.resolve();
 }
