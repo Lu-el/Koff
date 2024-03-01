501c010beb8ebf587783da65da52deab421a152b
Lu-el <lubov.elunina@mail.ru>
2024-03-01 10:13:28 +0400

    05-02 20min

--- features/LikeButton/LikeButton.js
+++ features/LikeButton/LikeButton.js
@@ -1,8 +1,10 @@
+import { FavoriteService } from "../../services/StorageService";
 import { likeSvg } from "../likeSvg/likeSVG";
 
 export class LikeButton {
   constructor(className) {
     this.className = className;
+    this.favoriteService = new FavoriteService()
   }
 
   create(id) {
@@ -10,8 +12,18 @@ export class LikeButton {
     button.classList.add(this.className);
     button.dataset.id = id;
 
+    if (this.favoriteService.check(id)) {
+      button.classList.add(`${this.className}_active`);
+    }
+
     button.addEventListener('click', () => {
-      console.log('Добавить в избранное');
+      if (this.favoriteService.check(id)) {
+        this.favoriteService.remove(id);
+        button.classList.remove(`${this.className}_active`);
+      } else {
+        this.favoriteService.add(id);
+        button.classList.add(`${this.className}_active`);
+      }
     });
 
     likeSvg().then((svg) => {
